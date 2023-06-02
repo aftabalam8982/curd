c79f4f181501f78e3011a5705525c9ca3dc10009
aftab-1194 <aftab.alam@classicinformatics.com>
2023-06-02 21:08:04 +0530

    added some dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
-import { useState } from 'react';
-import './App.css';
-import RegistrationForm from './RegistrationForm';
-import Table from './Table';
-
-export interface formValues{
-  firstName:string
-  lastName:string
-  email:string
-  education:string
-  experience:string
-  gender:string
-}
+import { BrowserRouter } from 'react-router-dom';
+import Form from './forms/Form';
 
 const App =():JSX.Element =>{
-  const [values, setValues] = useState<formValues[]>([{
-    firstName: '',
-    lastName: '',
-    email: '',
-    education: '',
-    experience: '',
-    gender: '',
-  }]);
-
-const submitData = (formData:formValues) => {
-  setValues([...values,formData])
-}
   return (
     <div >
-      <RegistrationForm submitData={submitData}/>
-      <Table formData={values} />
+      <BrowserRouter>
+      <Form/>
+      </BrowserRouter>
+      
     </div>
   );
 }
